96a20f1d80ecbea18af767210bebfb1dcabcce72
victormc <victortenemc@gmail.com>
2023-09-17 11:23:59 +0100

    Añadiendo footdistrict y quitando asos

--- compobador_ofertas.py
+++ compobador_ofertas.py
@@ -8,13 +8,14 @@ from celeryapp import celery
 from requests_html import HTMLSession
 from urllib.parse import urlparse
 from index import flash
+
 session = HTMLSession()
 
 
 def buscar_tienda(url):
     # Los nombres y los diferentes atributos que tienes la paginas para etiquetar que un producto esta rebajado
     tiendas = [
-        ("asos", 'discountPercentage":0'),
+        ("footdistrict", 'old-price'),
         ("boohooman", 'price-sales-red'),
         ("bstn", '<span class="percentage-savings">-')
     ]
@@ -82,29 +83,16 @@ def check_sale(email_usu, urls_usu):
             # Se busca si el atributo de descuento de la pagina esta presente en el contenido
             on_sale = content_page.text.find(key_to_find)
 
-            if key_to_find == "price-sales-red" or key_to_find == '<span class="percentage-savings">-':
-                if on_sale == -1:
-                    print(f"Todavia no esta rebajado: {url}")
-                    rebajados = False
-                else:
-                    # Verificar que no se haya enviado antes
-                    if url not in rebajados_lista:
-                        print(f"Esta rebajado: {url}")
-                        envio_email.enviar_email(email_usu, tienda, url)
-                        # Marcar como procesado
-                        rebajados_lista.append(url)
-
+            if on_sale == -1:
+                print(f"Todavia no esta rebajado: {url}")
+                rebajados = False
             else:
-                if on_sale == -1:
-                    # Verificar que no se haya enviado antes
-                    if url not in rebajados_lista:
-                        print(f"Esta rebajado: {url}")
-                        envio_email.enviar_email(email_usu, tienda, url)
-                        # Marcar como procesado
-                        rebajados_lista.append(url)
-                else:
-                    print(f"Todavia no esta rebajado: {url}")
-                    rebajados = False
+                # Verificar que no se haya enviado antes
+                if url not in rebajados_lista:
+                    print(f"Esta rebajado: {url}")
+                    envio_email.enviar_email(email_usu, tienda, url)
+                    # Marcar como procesado
+                    rebajados_lista.append(url)
 
         if not rebajados:
             # Si alguno no está rebajado, espera 10 minutos y se vuelve a ejecutar
